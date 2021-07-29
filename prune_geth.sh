b44f36a5933fc594508ecde3fb97ea793405114a
Ted Steen <ted.steen@gmail.com>
2021-07-29 18:13:00 +0300

    better bash

--- prune_geth.sh
+++ prune_geth.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 CONTAINER_NAME="rocketpool_eth1"
-GETH_VERSION=$( docker exec -i rocketpool_eth1 geth version | grep -Po "(?<=^Version: )[0-9\.]+" ) # See docker exec -i rocketpool_eth1 geth version
+GETH_VERSION=$( docker exec -i $CONTAINER_NAME geth version | grep -Po "(?<=^Version: )[0-9\.]+" ) # See docker exec -i rocketpool_eth1 geth version
 ETH1_DATA_VOLUME=$( docker inspect -f '{{ json .Mounts }}' $CONTAINER_NAME | jq -r '.[] | select(.Destination=="/ethclient").Source' )
 
+fail() {
+    MESSAGE=$1
+    RED='\033[0;31m'
+    >&2 echo -e "\n${RED}**ERROR**\n$MESSAGE"
+    exit 1
+}
+
+start_geth() {
+    echo "Starting geth..."
+    docker start $CONTAINER_NAME || fail "Could not start $CONTAINER_NAME"
+}
+
+trap start_geth EXIT
+
 echo "Stopping geth..."
-docker stop $CONTAINER_NAME
+docker stop $CONTAINER_NAME || fail "Could not stop $CONTAINER_NAME"
 
 echo "Running the pruner..."
-docker run --rm -it -v $ETH1_DATA_VOLUME:/ethclient ethereum/client-go:v$GETH_VERSION snapshot prune-state --goerli --datadir /ethclient/geth
+docker run --rm -it -v $ETH1_DATA_VOLUME:/ethclient ethereum/client-go:v$GETH_VERSION snapshot prune-state --goerli --datadir /ethclient/geth || fail "Failed to start pruning"
 echo "Done pruning!"
-
-echo "Starting geth..."
-docker start $CONTAINER_NAME
